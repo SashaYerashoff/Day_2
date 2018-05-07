68bbcdcc67e857c909f203995b572ecbdf549e0e
SashaYerashoff <a.yerashoff@gmail.com>
2018-05-07 20:01:39 +0300

    Day_2
    Testa Repo

---
+++ Day_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dati;
+            Console.WriteLine("ludzu ievadi savus dzimsanas datus: ");
+            dati = Console.ReadLine();
+            Console.WriteLine("Jus esat dzimis: " + dati);
+            Console.ReadLine();
+        }
+    }
+}
